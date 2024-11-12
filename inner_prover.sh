3b393bbcb2044fff72b68ccedacc53f2cc231fec
Gatzsu <162097227+Gatzsu@users.noreply.github.com>
2024-11-13 00:43:10 +0700

    Update inner_prover.sh

--- inner_prover.sh
+++ inner_prover.sh
@@ -1,8 +1,8 @@
 # use your own Lumoz reward_address
-reward_address=0xxxxx...
+reward_address=0x028696617f61e4a9ff82e6c505c80380059c3b8b
 
 # set your own custom name
-custom_name="myprover"
+custom_name="gatzsu"
 
 pids=$(ps -ef | grep moz_prover | grep -v grep | awk '{print $2}')
 if [ -n "$pids" ]; then
